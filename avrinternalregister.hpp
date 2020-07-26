526015a01377979569886cb7cd2aff8185f2d814
Johannes Wilde <git@jowil.de>
2020-07-26 16:28:10 +0200

    Remove unneeded define, which was previously used for QtCreator visualization only.

--- avrinternalregister.hpp
+++ avrinternalregister.hpp
@@ -1,11 +1,6 @@
 #ifndef AVRINTERNALREGISTER_HPP
 #define AVRINTERNALREGISTER_HPP
 
-// this is only for the QtCreator to find the correct highlighting - this is already defined somewhere before
-#ifndef __AVR_ATmega328P__
-#define __AVR_ATmega328P__
-#endif
-
 #include <avr/io.h>
 #include <avr/sfr_defs.h>
 #include <stdint.h>
