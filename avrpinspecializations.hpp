d00e28a3c10e3a3a5e87f64c322ab2782b3ee77e
Johannes Wilde <git@jowil.de>
2020-08-03 17:43:59 +0200

    Add one comment.

--- avrpinspecializations.hpp
+++ avrpinspecializations.hpp
@@ -3,6 +3,21 @@
 
 #include "avrpin.hpp"
 
+/** AvrPinSpecializations - Classes abstracting the internal interface for
+ * using Avr pins.
+ * These classes are stripped to their bare minimum and support only the
+ * necessary functionality for the respective type.
+ * Please note that it is currently not supported to change the type of
+ * the pin during execution.
+ *
+ * As however all of these are template classes, one could create typedefs
+ * of different types [e.g. AvrPinInput and AvrPinOutput] for the same pin
+ * and re-"initialize()" those anywhere in the program - please note however
+ * that you as the user are responsible for having these classes correctly
+ * initialized before using any of their member functions.
+ */
+
+
 template<typename AvrIoRegister, unsigned pinNumber_>
 struct AvrPinInput : private AvrPin<AvrIoRegister, pinNumber_>
 {
